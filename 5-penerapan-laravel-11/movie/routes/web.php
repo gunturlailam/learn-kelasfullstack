15c00c7909bd11649e9f95bd6213f828d8adc5cb
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2026-05-10 18:45:54 +0700

    feat: router - mengubah data dengan method put dan cara kirim melalui form

--- 5-penerapan-laravel-11/movie/routes/web.php
+++ 5-penerapan-laravel-11/movie/routes/web.php
@@ -27,10 +27,20 @@ Route::get('/movie', function () use ($movies) {
     echo "</ul>";
 });
 
-Route::post('/movie', function () {
+Route::post('/movie', function () use ($movies) {
     $movies[] = [
         'title' => request('title'),
         'year' => request('year'),
         'genre' => request('genre'),
     ];
+
+    return $movies;
+});
+
+Route::put('/movie', function () use ($movies) {
+    $movies[2]['title'] = request('title');
+    $movies[2]['year'] = request('year');
+    $movies[2]['genre'] = request('genre');
+
+    return $movies;
 });
